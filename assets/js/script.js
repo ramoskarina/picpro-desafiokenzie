f08f057027077bfae583a07de07024950b1c0fd0
Karina Ramos <61384905+ramoskarina@users.noreply.github.com>
2022-04-12 16:32:59 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const listaOpcoes = document.querySelector(".secaoDetalhesConta__opcoesTransacoes")
+
+listaOpcoes.addEventListener("click", identificarOpcoes)
+
+function identificarOpcoes(){
+  console.log("oi")
+}
